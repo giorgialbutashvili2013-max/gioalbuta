6961e1368911a9935d1366a397d4c0340f562a5f
giorgialbutashvili2013-max <giorgialbutashvili2013@gmail.com>
2025-11-07 17:16:01 +0400

    second commit

---
+++ day 005/homework/phython.py
@@ -0,0 +1,15 @@
+#and-და
+#or-ან
+#not-არა
+
+
+print("false and false") #false
+print("false and true") #false
+print("true and false") #false
+print("true and true") #true
+print("true or false") #true
+print("true or true") #true
+
+
+("true")
+("false")
---
+++ day 007/homework/phython.py
@@ -0,0 +1,26 @@
+#sepuencing:კოდი თანმიმდევრობით სრულდება
+#interation:გამეორება/ციკლი
+#selection:არჩევნის გაკეთება,რაღაც პირობის მიხედვით
+
+#for ციკლი მუშაობს რომ რაიმე სახელი გავამეოროთ რამდენჯერაც გვინდა
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(15):
+    print("meat")
+
+
+for i in range(5):
+    print("beso")
+
+
+for i in range(5):
+    print("manana")
+
+
+for i in range(5):
+    print("eto")
+
+
