5a2892e4385d62a5a9a986566d727791ac188bb7
giorgialbutashvili2013-max <giorgialbutashvili2013@gmail.com>
2025-11-08 15:51:24 +0400

    second commit

---
+++ day 011/classwork/phython.py
@@ -0,0 +1,24 @@
+number=int(input(14))
+    
+if number>10:
+    print("didia")
+elif number==10:    
+    print("zustad 10")
+else:
+    print("patara")
+
+
+age=int(input(12))
+
+if age<13:
+    print("child")
+elif age<18:
+    print("teenager")
+else:
+    print("adult")
+
+
+
+
+
+
